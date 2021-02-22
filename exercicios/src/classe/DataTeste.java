aa4943fe9af4cf8bcb5891853cb44cb78e6e1e8c
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-02-22 10:45:43 -0300

    Classe Data criada como desafio

---
+++ exercicios/src/classe/DataTeste.java
@@ -0,0 +1,20 @@
+package classe;
+
+public class DataTeste {
+
+	public static void main(String[] args) {
+		
+		Data data1 = new Data();
+		data1.dia = 8;
+		data1.mes = 11;
+		data1.ano = 2020;
+		
+		Data data2 = new Data();
+		data2.dia = 11;
+		data2.mes = 8;
+		data2.ano = 1932;
+		
+		System.out.printf("%d/%d/%d\n", data1.dia, data1.mes, data1.ano);
+		System.out.printf("%d/%d/%d\n", data2.dia, data2.mes, data2.ano);
+	}
+}
