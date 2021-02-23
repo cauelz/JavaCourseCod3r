17d943d02992ce97ff80b53f29da0cd67a79b8b8
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-02-23 08:04:32 -0300

    Membros de Classe vs Instância

---
+++ exercicios/src/classe/AreaCircTeste.java
@@ -0,0 +1,25 @@
+package classe;
+
+public class AreaCircTeste {
+
+	public static void main(String[] args) {
+				
+		AreaCirc a1 = new AreaCirc(10);
+		AreaCirc a2 = new AreaCirc(5);
+		
+//		AreaCirc.pi = 3.1415;
+		
+		System.out.println(a1.area());
+		System.out.println(a2.area());
+		
+//		AreaCirc.pi = 2;
+//		
+//		System.out.println(a1.area());
+//		System.out.println(a2.area());
+	}
+	
+	
+
+	
+	
+}
