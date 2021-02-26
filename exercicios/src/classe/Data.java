55d405f137efbbdf22f401a6d4e70bb37ed55243
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-02-26 13:48:18 -0300

    Aula sobre This

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -8,19 +8,23 @@ public class Data {
 	
 	
 	Data() {
-		dia = 1;
-		mes = 1;
-		ano = 1970;
+		
+		// esta forma só serve para chamar um construtor dentro de outro construtor.
+		this(1, 2, 1980);
 	}
 	
-	Data(int diaInicial, int mesInicial, int anoInicial) {
-		dia = diaInicial;
-		mes = mesInicial;
-		ano = anoInicial;
+	Data(int dia, int mes, int ano) {
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
 	}
 	
 	String obterDataFormatada() {
-		return String.format("%d/%d/%d", dia, mes, ano);
+		return String.format("%d/%d/%d", this.dia, this.mes, this.ano);
+	}
+	
+	void imprimirDataFormatada() {
+		System.out.println(this.obterDataFormatada());
 	}
 	
 }
