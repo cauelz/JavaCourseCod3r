0cd93ef05a0317b959cdafea34974cf527a18ff7
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-02-27 12:18:36 -0300

    Aula de Arrays

---
+++ exercicios/src/arrays/Exercicio.java
@@ -0,0 +1,32 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class Exercicio {
+
+	public static void main(String[] args) {
+		
+		double[] notasAlunoA = new double[3];
+		double notasAlunoB[] = new double[3];
+		double []notasAlunoC = new double[3];
+		System.out.println(Arrays.toString(notasAlunoA));
+		
+		notasAlunoA[0] = 8.9;
+		notasAlunoA[1] = 10; // veja que temos aqui uma conversão implícita para o tipo inteiro
+		notasAlunoA[2] = 3.3;
+		
+		System.out.println(notasAlunoA);
+		System.out.println(Arrays.toString(notasAlunoA));
+		
+		double total = 0;
+		for(int i = 0; i < notasAlunoA.length; i++) {
+			total += notasAlunoA[i];
+		}
+		double media = total/notasAlunoA.length;
+		
+		System.out.println(media);
+		
+		double[] notasAlunoD = {1, 6, 7, 8, 3}; // outra forma de declarar um array, não confundir com Javascript.
+		
+	}
+}
