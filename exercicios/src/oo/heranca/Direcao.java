bfefe587867ccac205963d022f7574d4690025da
Caue Lasagno Zaratin <caue.lasagno.zaratin@gmail.com>
2021-03-11 10:12:26 -0300

    Aula ENUM

---
+++ exercicios/src/oo/heranca/Direcao.java
@@ -0,0 +1,15 @@
+package oo.heranca;
+
+public enum Direcao {
+	
+	/* ENUM:
+	 * 
+	 * Eles também definem um tipo de dados!
+	 * */
+
+	// apesar de não representar um tipo associado as palavras abaixo
+	// está associado a elas os valores 0, 1, 2, 3
+	NORTE, LESTE, SUL, OESTE;
+	
+	
+}
